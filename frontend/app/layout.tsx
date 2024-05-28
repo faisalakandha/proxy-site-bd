c277d35e9599ca2218a85163162fbd516499734d
Faisal Akandha <faisalakandha24@gmail.com>
2024-05-29 04:22:49 +0600

    Bottom ad sticky added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Footer, ThemeModeScript } from "flowbite-react";
@@ -5,19 +7,25 @@ import "./globals.css";
 import { SiteFooter } from "@/src/Footer";
 import { Header } from "@/src/Header";
 import { HeaderMobile } from "@/src/Header-Mobile";
+import SiteDownAdFooter from "@/src/Ads/SiteDownAdFooter";
+import { CircleX } from "lucide-react";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Croxy Proxy",
-  description: "Unlock any website",
-};
+// export const metadata: Metadata = {
+//   title: "Croxy Proxy",
+//   description: "Unlock any website",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const [bottomAdClose, setBottomAdClose] = useState(true);
+
   return (
     <html lang="en">
       <head>
@@ -32,6 +40,14 @@ export default function RootLayout({
         </div>
         <div>
           {children}
+        </div>
+        <div style={{ borderTop: '1px solid grey', display: bottomAdClose ? 'block' : 'none', zIndex:'80' }} className='hidden md:block sticky bottom-0 py-2 bg-white'>
+          <CircleX onClick={() => { setBottomAdClose(false) }} style={{ marginTop: '-20px', borderRadius: '20px', cursor: 'pointer' }} className="ml-auto mr-3 text-white bg-black" />
+          <div className="flex items-center justify-center">
+            <div style={{ height: '90px', width: '720px' }} className="bg-orange-400">
+
+            </div>
+          </div>
         </div>
         <SiteFooter />
       </body>
